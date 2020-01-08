379697c144ac605733b79e98fc4b63f962e78f4c
HeyWeCome <heywecome@163.com>
2020-01-08 23:21:41 +0800

    配置了首页

--- src/main/java/com/heywecome/config/MyMvcConfig.java
+++ src/main/java/com/heywecome/config/MyMvcConfig.java
@@ -1,9 +1,7 @@
 package com.heywecome.config;
 
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -18,9 +16,13 @@ import java.util.Locale;
 
 // 如果我们翔diy一些定制化的功能，只要写了这个组件，然后把它交给springboot，springboot就会帮我们自动装配
 // 拓展springmvc dispatchservlet
-
 // 如果我们要扩展springmvc，官方建议我们这样去做
 @Configuration
 public class MyMvcConfig implements WebMvcConfigurer{
 
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
 }
