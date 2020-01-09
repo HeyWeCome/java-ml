52ccddcfc33fb85b21991c454db3762338d1ffa6
HeyWeCome <heywecome@163.com>
2020-01-09 23:54:47 +0800

    国际化设置

--- src/main/java/com/heywecome/config/MyMvcConfig.java
+++ src/main/java/com/heywecome/config/MyMvcConfig.java
@@ -1,11 +1,11 @@
 package com.heywecome.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Locale;
 
 /**
  * @description: 全面拓展 springmvc
@@ -24,5 +24,11 @@ public class MyMvcConfig implements WebMvcConfigurer{
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+    }
+
+    // 自定义的国际化组件
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
