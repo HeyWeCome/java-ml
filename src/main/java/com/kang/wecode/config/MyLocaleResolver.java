58bf22009e51df51d100303b2af35319d32ea500
HeyWeCome <heywecome@163.com>
2020-02-28 22:10:19 +0800

    国际化配置

--- src/main/java/com/kang/wecode/config/MyLocaleResolver.java
+++ src/main/java/com/kang/wecode/config/MyLocaleResolver.java
@@ -1,6 +1,7 @@
 package com.kang.wecode.config;
 
 import org.springframework.web.servlet.LocaleResolver;
+import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,11 +14,25 @@ import java.util.Locale;
  * @version: 1.0
  */
 public class MyLocaleResolver implements LocaleResolver {
+    // 解析请求
     @Override
     public Locale resolveLocale(HttpServletRequest request) {
-        return null;
+        // 获取请求中的语言参数
+        String language = request.getParameter("language");
+        // 如果没有则使用默认的
+        Locale locale = Locale.getDefault();
+
+        // 如果请求的链接携带了国际化的参数
+        if(!StringUtils.isEmpty(language)){
+            // 分割出国家地区
+            String[] split = language.split("_");
+            // 国家，地区
+            locale = new Locale(split[0], split[1]);
+        }
+        return locale;
     }
 
+    //
     @Override
     public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
 
