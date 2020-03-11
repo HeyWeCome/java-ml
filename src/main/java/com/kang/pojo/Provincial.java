758b4b6b29184c5b912047952e510eaccb17367d
HeyWeCome <heywecome@163.com>
2020-03-11 20:35:46 +0800

    pojo设计

---
+++ src/main/java/com/kang/pojo/Provincial.java
@@ -0,0 +1,20 @@
+package com.kang.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @description:
+ * @author: HeyWeCome
+ * @createDate: 2020/3/11 20:10
+ * @version: 1.0
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Provincial {
+    String id;      // 编号
+    String name;    // 学院名
+}
--- src/main/java/com/kang/pojo/User.java
+++ src/main/java/com/kang/pojo/User.java
@@ -14,6 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
-    String userName;              // 用户id，采用 UUID
-    String password;            // 用户姓名
+    String id;                  // 用户的采用 UUID
+    String name;                // 用户姓名，
+    String account;             // 账号
+    String password;            // 用户密码
+    String email;               // 电子邮箱
+    String phoneNumber;         // 手机号码
+    String address;             // 地址
+    String photo;               // 头像地址
+    String introduction;        // 个人简介
+    int voluntarySchool;        // 报考院校的编号
+    int age;                    // 年龄
+    int sex;                    // 性别，男生 1 ，女生 0
 }
