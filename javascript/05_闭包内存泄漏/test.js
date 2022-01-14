283c35ba5791984036c47b9d76acc03ced32380d
heywecome <heywecome@163.com>
2022-01-14 11:49:38 +0800

    测试

--- javascript/05_闭包内存泄漏/test.js
+++ javascript/05_闭包内存泄漏/test.js
@@ -1,11 +1,2 @@
-var foo = (num1, num2) => {
-  return num1+num2
-}
-
-const full = ({ first, last }) => first + ' ' + last;
-
-const person = {first: "tom", last: "jack"};
-console.log(full(person))
-
-let [arr1, ...arr2] = [1,2,3,4]
-console.log(arr2)
+const a = [...'hello'];
+console.log(a)
