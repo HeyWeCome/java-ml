e5dcb3d289c8039ce08199ecf35905c1936209a4
heywecome <heywecome@163.com>
2022-02-22 21:11:26 +0800

    作用域插槽补充

--- vue/02_learn_component/src/10_作用域插槽使用/App.vue
+++ vue/02_learn_component/src/10_作用域插槽使用/App.vue
@@ -1,12 +1,38 @@
 <template>
   <div>
+    <show-names :names="names">
+      <template v-slot:heywecome="slotProps">
+        <button>{{slotProps.item}}-{{slotProps.index}}</button>
+      </template>
+    </show-names>
 
+    <!-- 默认插槽的省略写法：独占默认插槽的缩写。如果有其他的具名插槽，不能这样写，必须要template-->
+    <show-names :names="names" v-slot="slotProps">
+      <button>{{slotProps.item}}-{{slotProps.index}}</button>
+    </show-names>
+
+    <show-names :names="names">
+      <template #heywecome="slotProps">
+        <h1>{{slotProps.item}}-{{slotProps.index}}</h1>
+      </template>
+    </show-names>
   </div>
 </template>
 
 <script>
+  // import ChildCpn from "./ChildCpn.vue";
+  import ShowNames from "./ShowNames.vue";
+
   export default {
-    
+    components: {
+      // ChildCpn,
+      ShowNames
+    },
+    data() {
+      return {
+        names: ['heywecome', 'james', 'curry']
+      }
+    },
   }
 </script>
 
---
+++ vue/02_learn_component/src/10_作用域插槽使用/ShowNames.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <template v-for="(item, index) in names" :key="item">
+      <slot name="heywecome" :item="item" :index="index"></slot>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      names: {
+        type: Array,
+        default: () => []
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
